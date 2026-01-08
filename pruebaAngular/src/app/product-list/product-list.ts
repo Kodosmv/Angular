7052b359b72d9182182b265636eaf1cfcf39ee74
Kodosmv <soledat10@gmail.com>
2026-01-08 16:18:36 +0100

    arreglos

--- pruebaAngular/src/app/product-list/product-list.ts
+++ pruebaAngular/src/app/product-list/product-list.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from '../interfaces/i-product';
 
 @Component({
   selector: 'app-product-list',
@@ -8,5 +9,23 @@ import { Component } from '@angular/core';
 })
 export class ProductList {
   title = 'Mi lista de productos';
-  headers = { desc: 'Producto', price: 'Precio', avail: 'Disponible' };
+  headers = { image: 'Image', desc: 'Producto', price: 'Precio', avail: 'Disponible' };
+  products: IProduct[] = [
+    {
+      id: 1,
+      desc: 'SSD hard drive',
+      avail: new Date('2016-10-03'),
+      price: 75,
+      imageUrl: '/ss1.jpg',
+      rating: 5,
+    },
+    {
+      id: 2,
+      desc: 'LGA1151 Motherboard',
+      avail: new Date('2016-09-15'),
+      price: 96.95,
+      imageUrl: '/motherboard.jpg',
+      rating: 4,
+    },
+  ];
 }
