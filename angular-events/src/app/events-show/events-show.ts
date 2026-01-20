54ecb7c975f5eb3b481bd5503af651269e92ff8b
Kodosmv <soledat10@gmail.com>
2026-01-20 18:34:33 +0100

    subir a main

--- angular-events/src/app/events-show/events-show.ts
+++ angular-events/src/app/events-show/events-show.ts
@@ -2,17 +2,32 @@ import { Component } from '@angular/core';
 import { IEvent } from '../../interfaces/i-event';
 import { CurrencyPipe } from '@angular/common';
 import { DatePipe } from '@angular/common';
+import { TitleCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { EventFilterPipe } from '../pipes/event-filter-pipe';
 
 @Component({
   selector: 'app-events-show',
-  imports: [CurrencyPipe, DatePipe],
+  imports: [CurrencyPipe, DatePipe, TitleCasePipe, FormsModule, EventFilterPipe],
   templateUrl: './events-show.html',
   styleUrl: './events-show.css',
 })
 export class EventsShow {
+  orderPrice() {
+   /* this.events = this.events.sort(a, b){
+      if(a.price>b.price){
+        return 1;
+      }
+    } );
+     */
+  }
+  orderDate() {
+    throw new Error('Method not implemented.');
+  }
+  search = '';
   events: IEvent[] = [
     {
-      title: 'Foto uno',
+      title: 'foto uno',
       image: 'img1.jpg',
       date: '2004-05-08',
       description: 'noseque',
